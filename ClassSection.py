fadb5946c21ce295b3be9b21075e306b9734742d
alecmagnani <alecmagnani@gmail.com>
2017-04-26 00:28:01 -0400

    date and time scraping

--- ClassSection.py
+++ ClassSection.py
@@ -3,13 +3,10 @@ class ClassSection:
     def __init__(self, title, link):
         self.title = title
         self.link = link
-        self.days = []
-        self.time = ""
+        self.date_time = ""
 
-    def set_timeslot(self, days, time):
-        self.days = days
-        self.time = time
+    def set_timeslot(self, date_time):
+        self.date_time = date_time
 
     def display(self):
-        # print(self.title + " " + self.days + " " + self.time)
-        print(self.title)
+        print(self.title + " " + self.date_time)
--- pitt-rooms.py
+++ pitt-rooms.py
@@ -1,7 +1,7 @@
-import requests
-import re
+import requests, re
+import webbrowser
 from ClassSection import ClassSection
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 
 BULDING_CODES = { 'ALLEN':'Allen Hall', 'ALUM':'Alumni Hall', 'BELLH':'Bellefield Hall',
             'BENDM':'Benedum Hall','BSTWR':'Biomedical Science Tower',
@@ -15,6 +15,14 @@ BULDING_CODES = { 'ALLEN':'Allen Hall', 'ALUM':'Alumni Hall', 'BELLH':'Bellefiel
             'THACK':'Thackeray Hall', 'THAW':'Thaw Hall', 'TREES':'Trees Hall',
             'WWPH':'Wesley W. Posvar Hall' }
 
+# DAY_CODES = { 'Mo':'Monday', 'Tu':'Tuesday', 'We':'Wednesday',
+# 'Th':'Thursday', 'Fr':'Friday','Sa':'Saturday', 'Su':'Sunday'}
+
+DAY_CODES = [ "Mo ", "Tu ", "We ", "Th ", "Fr ", "Sa ", "Su "]
+
+TITLE_IGNORE_LIST = ["M.S. Thesis"]
+BLACKLIST = ["Web", "WEB", "WWW", "web", "TBA"]
+
 CURRENT_TERM = "2174"
 OFFSET_VAL = 15
 sections = []
@@ -23,7 +31,6 @@ for PAGE_NUM in range(0, 1):
     URL = "http://www.courses.as.pitt.edu/results-title.asp?TITL=+%2F&TERM=" + CURRENT_TERM + "&offset=" + str(OFFSET_VAL * PAGE_NUM)
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'lxml')
-
     elems = (soup.find_all('tr', {'class':['even', 'odd']}))
 
     for elem in elems:
@@ -35,9 +42,25 @@ for PAGE_NUM in range(0, 1):
         link = str(re.findall('"([^"]*)"', str(link)))
         link = str(link).strip("['")
         link = str(link).strip("']")
-        link = "courses.as.pitt.edu/" + link
+        link = str(link).replace("&amp;", "&")
+        link = "http://www.courses.as.pitt.edu/" + link
 
-        sections.append(ClassSection(strtitle, link))
+        if(strtitle not in TITLE_IGNORE_LIST):
+            sections.append(ClassSection(strtitle, link))
+
+for sec in sections:
+    url = sec.link
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    text = soup.find_all("td", class_="style1")
+    for t in text:
+        if any(bl in str(t) for bl in BLACKLIST):
+            sections.remove(sec)
+            break
+        elif any(day in str(t) for day in DAY_CODES) and any(building in str(t)for building in BULDING_CODES):
+            sec.set_timeslot(t.get_text())
+            break
 
 for sec in sections:
     sec.display()
