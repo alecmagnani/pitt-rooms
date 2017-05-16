04811e8eaac0ec7f00b868866a1a8419d820759d
alecmagnani <alecmagnani@gmail.com>
2017-05-16 16:16:45 -0400

    i think im doing SQL

--- pitt-rooms.py
+++ pitt-rooms.py
@@ -1,14 +1,10 @@
-import time, datetime
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import time, datetime, sqlite3
 from ClassSection import ClassSection
 
-# use creds to create a cleint to interact with the Google Drive API
-# scope = ['https://spreadsheets.google.com/feeds']
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-# client = gspread.authorize(creds)
-
-# sheet = client.open("pitt-courses_2174").sheet1
+sqlite_file = 'courses_2174.sqlite'
+table_name = 'courses'
+connection = sqlite3.connect(sqlite_file)
+cursor = connection.cursor()
 
 def convert_to_24hr(time):
     #format: "HH:MM XX"
@@ -26,35 +22,47 @@ def convert_to_24hr(time):
     return datetime.time( int(time[0]), int(time[1]), 0)
 
 
-def time_in_range(start, end, x):
+def time_in_range(time_range, x):
+    time_range = time_range.split("-")
+    start = convert_to_24hr(time_range[0])
+    end = convert_to_24hr(time_range[1])
     if start <= end:
         return start <= x <= end
     else:
         return start <= x or x <= end
 
+def clean_input(string):
+    return ''.join(char for char in string if char.isalnum())
+
 if __name__ == "__main__":
     _building = input("Enter a building: ")
-    _day = input("Enter a day (Leave blank to use today):  ")
+    _day = input("Enter a day (Leave blank to use today): ")
     _time = input("Enter a time( Leave blank to use now): ")
 
     if _building in ClassSection.BUILDING_CODES_REV:
         _building = ClassSection.BUILDING_CODES_REV[_building]
     elif _building not in ClassSection.BUILDING_CODES:
         print("Error - building " + _building + " not recognized.")
-
-    print(_building)
+    _building = clean_input(_building)
 
     if _day == "":
         _day = time.strftime("%A")
-        print(_day)
-    else:
-        print(ClassSection.DAY_DICT[_day])
+    if _day + " " not in ClassSection.DAY_CODES:
+        _day = ClassSection.DAY_DICT[_day]
 
     if _time == "":
         _time = datetime.datetime.now().time()
     else:
         _time = convert_to_24hr(_time)
 
-    start = convert_to_24hr("2:30 PM")
-    end = convert_to_24hr("3:45 PM")
-    print(time_in_range(start, end, _time))
+    cursor.execute('SELECT * FROM courses WHERE {cn}=?'.\
+            format(cn='building'), (_building,))
+
+    while True:
+        row = cursor.fetchone()
+
+        if row == None:
+            break
+
+        if _day in row[1] and time_in_range(row[2], _time):
+            print(row)
