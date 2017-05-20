207807c27f549ef93e872fa653cd62e4bb541db1
alecmagnani <alecmagnani@gmail.com>
2017-05-20 17:49:59 -0400

    can return list of busy classrooms. todo - generate list of empty?

--- pitt-rooms.py
+++ pitt-rooms.py
@@ -7,28 +7,35 @@ connection = sqlite3.connect(sqlite_file)
 cursor = connection.cursor()
 
 def convert_to_24hr(time):
-    #format: "HH:MM XX"
+    #format: "hh:MM XX" or "HH:MM" or datetime.time (HH:MM:SS)
+    if "PM" in time:
+        am_pm = "PM"
+    else:
+        am_pm = "AM"
+
     time = time.split(" ")
-    am_pm = "AM"
-
-    if len(time) > 1:
-        am_pm = time[1]
-
     time = time[0].split(":")
+    hour = int(time[0])
+    minute = int(time[1])
 
-    if am_pm == "PM":
-        time[0] = int(time[0]) + 12
+    if am_pm == "PM" and hour < 12:
+        hour += 12
 
-    return datetime.time( int(time[0]), int(time[1]), 0)
-
+    return datetime.time(hour, minute, 0)
 
 def time_in_range(time_range, x):
     time_range = time_range.split("-")
     start = convert_to_24hr(time_range[0])
     end = convert_to_24hr(time_range[1])
+
+    # print("START: " + str(start))
+    # print("END:   " + str(end))
+
     if start <= end:
+        # print(start <= x <= end)
         return start <= x <= end
     else:
+        # print(start <= x or x <= end)
         return start <= x or x <= end
 
 def clean_input(string):
@@ -43,6 +50,8 @@ if __name__ == "__main__":
         _building = ClassSection.BUILDING_CODES_REV[_building]
     elif _building not in ClassSection.BUILDING_CODES:
         print("Error - building " + _building + " not recognized.")
+        exit(1)
+
     _building = clean_input(_building)
 
     if _day == "":
@@ -62,7 +71,10 @@ if __name__ == "__main__":
         row = cursor.fetchone()
 
         if row == None:
+            print("End of results.")
             break
 
         if _day in row[1] and time_in_range(row[2], _time):
-            print(row)
+            print(row[4])
+
+    cursor.close()
